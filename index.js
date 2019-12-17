3c5110c97a446edcddd06193268b3c83c5c75a30
maruqyan <dmaruqyan@gmail.com>
2019-12-17 21:48:39 +0400

    homework

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
-const fs = require('fs');
+const http = require('http');
+const fs  = require('fs');
 const argv = require('minimist')(process.argv.slice(2));
+console.log(argv);
 
 
 /**
  *
  * @param {String} filePath - path your file
+ * @param {Object} res - response object
  */
 
-function readDir(filePath){
+
+function readDir(filePath,res){
   fs.readFile(`./${filePath}`,'utf-8',(err, data) => {
     if(err){
       console.log(`${filePath}.txt not found`);
     }else{
-      console.log(data);
+      res.end(data);
     }
 });
 }
 
+http.createServer((req,res) => {
+  if(!argv.e || argv.fileName){
+    res.writeHead(200);
+    res.end('hello world');
+  }
 
-switch(argv.f || argv.file || argv.c || argv.css){
-    case argv.f:readDir(`${argv.f}.txt`)
+  switch(argv.e || argv.fileName){
+    case 'txt':readDir(`hello.txt`,res);
     break;
-    case argv.file: readDir(`${argv.file}.txt`)
+    case 'html': readDir(`main.html`,res);
     break;
-    case argv.c:readDir(`${argv.c}.css`)
+    case 'hello':readDir(`${argv.fileName}.txt`,res);
     break;
-    case argv.css: readDir(`${argv.css}.css`)
+    case 'main': readDir(`${argv.fileName}.html`,res);
     break;
 }
-
-
-
-
-
+}).listen(3000);
--- test/strlength.js
+++
@@ -1,7 +0,0 @@
-exports.strLength = (str) => {
-  return str.length;
-};
-
-exports.getLength = (str) => {
-  return str.length;
-};
