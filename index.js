76362cd9a1e8162e0605bd605436cfad92b0d916
maruqyan <dmaruqyan@gmail.com>
2019-12-20 21:04:49 +0400

    created sendData function in index.js filr

--- index.js
+++ index.js
@@ -1,40 +1,51 @@
 const http = require('http');
-const fs  = require('fs');
-const argv = require('minimist')(process.argv.slice(2));
-console.log(argv);
+const fs = require('fs');
+const {e,fileName} = require('minimist')(process.argv.slice(2));
 
+/**
+ *
+ * @param {String} data - sending data
+ * @param {Number} statusCode - http status code
+ * @param {String} ContentType - type your content
+ * @param {Object} res - response Object
+ */
+
+function sendData(data,statusCode,ContentType,res){
+  res.writeHead(statusCode,{
+    'Content-Type':`${ContentType};charset=utf8`
+  });
+  res.write(data);
+  res.end();
+}
 
 /**
  *
  * @param {String} filePath - path your file
- * @param {Object} res - response object
+ * @param {Object} res - response Object
+ * @param {String} ext - your file extantion
  */
 
-
-function readDir(filePath,res){
-  fs.readFile(`./${filePath}`,'utf-8',(err, data) => {
+function readDirectoryData(filePath,ext,res){
+  fs.readFile(`${filePath}.${ext}`,'utf8',(err,data) => {
+    let wdata = data;
+    let statusCode = 200;
     if(err){
-      console.log(`${filePath}.txt not found`);
-    }else{
-      res.end(data);
+      wdata = 'File note found';
+      statusCode = 404;
     }
-});
+
+    sendData(wdata,statusCode,'text/html',res);
+  });
 }
 
 http.createServer((req,res) => {
-  if(!argv.e || argv.fileName){
-    res.writeHead(200);
-    res.end('hello world');
+  if(e && fileName){
+    readDirectoryData(fileName,e,res);
+  }else{
+    sendData('Hello World',200,'text/html',res);
   }
-
-  switch(argv.e || argv.fileName){
-    case 'txt':readDir(`hello.txt`,res);
-    break;
-    case 'html': readDir(`main.html`,res);
-    break;
-    case 'hello':readDir(`${argv.fileName}.txt`,res);
-    break;
-    case 'main': readDir(`${argv.fileName}.html`,res);
-    break;
-}
 }).listen(3000);
+
+
+
+
